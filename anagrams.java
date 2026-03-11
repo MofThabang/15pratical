905b32b9148a17d92add99e6a8f23703c9e0de56
MofThabang <4362668@myuwc.ac.za>
2026-03-11 14:57:55 +0200

    Update anagrams.java
    " angrams class is added"

--- anagrams.java
+++ anagrams.java
@@ -1,1 +1,13 @@
+import java.io;
+import java.util;
 
+public class Anagrams {
+  // storing signature by HashMap
+private static HashMap<String, ArrayList<String>> dictionary = new HahMap<>();
+
+  public satatic vid main(string[] args) {
+    readFile("ulysses.text");
+    printDictionary();
+    writeLatexFile("theAnagrams.text");
+  }
+  
